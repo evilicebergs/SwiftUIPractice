380086587f99dae1737d27c0e87c14766fe717e4
evilicebergs <voiced.fair-0a@icloud.com>
2025-01-25 15:10:51 +0200

    Initial Commit

---
+++ SwiftUIPractice/SwiftUIPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIPracticeApp.swift
+//  SwiftUIPractice
+//
+//  Created by Artem Golovchenko on 2025-01-25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
